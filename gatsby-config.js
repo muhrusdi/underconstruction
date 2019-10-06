aabb20db23275daeb61270eb074351bd10a413bb
Muhammad Rusdi <muhammadrusdi@icloud.com>
2019-10-06 14:41:56 +0000

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#333`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/dauroh-logo-4.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
